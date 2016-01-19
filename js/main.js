d7cf6975836fefdaa01d958c86d07aba240665c5
reg <reg.gamalo@gmail.com>
2016-01-19 19:09:37 +0800

    Update on video.

--- js/main.js
+++ js/main.js
@@ -3,50 +3,62 @@ var hosted_video = function(){
 	this.context = this.canvas.getContext('2d');
 	this.video_element = document.querySelector('#vid');
 	this.counter = 1;
+	this.renderimage();
 	this.events();
-	this.imagerender();
 }
 
-hosted_video.prototype.imagerender = function(){
+hosted_video.prototype.renderimage = function(){
 	var _this = this;
-	this.interval = setInterval(function(){ 
-		base = new Image();
-		base.src = 'https://rmarepo.richmediaads.com/140/images/autoplay/0j82wfm9529/image_'+ _this.counter +'.jpg';
-		base.onload = function(){
-			_this.context.drawImage(base, 0, 0);
-		}
-		_this.counter == 150 ? _this.counter = 1 : '';
-		_this.counter++;
-	}, 100)
+	// if(/Mobi/.test(navigator.userAgent)){ //this code is use to check for mobile devices, if mobile, then render images, else play video.
+		this.interval = setInterval(function(){ 
+			base = new Image();
+			base.src = 'https://rmarepo.richmediaads.com/140/images/autoplay/0j82wfm9529/image_'+ _this.counter +'.jpg';
+			base.onload = function(){
+				_this.context.drawImage(base, 0, 0);
+			}
+			_this.counter == 150 ? _this.counter = 1 : '';
+			_this.counter++;
+		}, 100)
+	// }else{
+	// 	this.rendervideo();
+	// }
+}
+
+hosted_video.prototype.rendervideo = function(){
+	var _this = this;
+	clearInterval(_this.interval);
+	// this.canvas.className = 'player hidden';
+	document.querySelector('#btnplay').className = 'hidden';
+	// this.source = document.createElement('source');
+	// this.source.setAttribute('src', 'https://rmarepo.richmediaads.com/140/videos/GALAXY_S4_Official.mp4');
+	// this.video_element.appendChild(this.source);
+	this.video_element.className = '';
+	this.context.drawImage(_this.video_element, 0, 0);
+
+	if(this.video_element.paused || this.video_element.ended){
+		this.video_element.play();
+	}else{
+		this.video_element.pause();
+	}
 }
 
 hosted_video.prototype.events = function(){
 	var _this = this;
 	document.querySelector('#player').addEventListener('click', function(){
-		clearInterval(_this.interval);
-		document.querySelector('#vid').className = '';
-		_this.video.play();
+		_this.rendervideo();
 	});
 
 	_this.video_element.addEventListener('click', function(){
-		_this.context.drawImage(this, 0, 0);
-		if(this.paused || this.ended){
-			this.play();
-		}else{
-			this.pause();
-		}
+		_this.rendervideo();
+	});
+
+	document.querySelector('#btnplay').addEventListener('click', function(){
+		_this.rendervideo();
 	});
 }
 
-hosted_video.prototype.video ={
-	play : function(){
-		var video = document.querySelector('#vid');
-		video.play();
-	},
-	pause: function(){
-		this.video_element.pause();
-	}
-}
-
 h = new hosted_video();
+
+
+
 
